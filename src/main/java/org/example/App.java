db8bc1c9829c4856a7b2da716a50bbc1ca582f3f
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2025-11-20 11:14:31 +0200

    Update App

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,23 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class App {
 
     public static void main(String[] args) {
 
         System.out.println("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
+        int limit = askInt();
+
+        for (int i = 1; i <= limit; i++) {
             System.out.println("i = " + i);
         }
+    }
+
+    public static int askInt() {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        return input.nextInt();
     }
 }
