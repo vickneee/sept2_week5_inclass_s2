97b0a1c3aeb4a1c1439166cce7cc7125539925c7
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2025-11-20 17:08:36 +0200

    Update to use Logger

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,23 +1,30 @@
 package org.example;
 
 import java.util.Scanner;
+import java.util.logging.Logger;
 
 public class App {
 
+    private static final Logger logger = Logger.getLogger(App.class.getName());
+
+
     public static void main(String[] args) {
 
-        System.out.println("Hello and welcome!");
+        logger.info("Hello Application started");
 
         int limit = askInt();
+        String result = "i = ";
 
         for (int i = 1; i <= limit; i++) {
-            System.out.println("i = " + i);
+            int finalI = i;
+            logger.info(() -> String.format(result, finalI));
         }
     }
 
     public static int askInt() {
         Scanner input = new Scanner(System.in);
-        System.out.print("Enter a number: ");
+        String userInput = "Enter a number: ";
+        logger.info(() -> String.format(userInput));
         return input.nextInt();
     }
 }
